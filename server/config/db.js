5bd2e584f7104a464265144a33e86cd3c4761c0d
hrishik123 <95434694+hrishik123@users.noreply.github.com>
2024-05-28 15:27:09 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 try {
-  mongoose.connect("mongodb://localhost:27017/Ecommerce", {
+  mongoose.connect("mongodb+srv://Hrishik:123Hrishik@ecom.awllnis.mongodb.net/?retryWrites=true&w=majority&appName=ecom", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
